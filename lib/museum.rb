a5907b304648c1b83d16c444ab6c92156c9cc8d8
msimon42 <msimon@uab.edu>
2019-10-23 09:47:44 -0600

    Add museum class

---
+++ lib/museum.rb
@@ -0,0 +1,16 @@
+class Museum
+  attr_reader :name, :exhibits
+
+  def initialize(name)
+    @name = name
+    @exhibits = Array.new
+  end
+
+  def add_exhibit(exhibit)
+    @exhibits << exhibit
+  end
+
+  def recommend_exhibits(patron)
+    @exhibits.find_all {|exhibit| patron.interests.include?(exhibit.name)}
+  end
+end
