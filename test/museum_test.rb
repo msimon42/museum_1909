32452bae0ebe75e1e419d365041d20027c152b3a
msimon42 <msimon@uab.edu>
2019-10-23 09:47:17 -0600

    Fix typos

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -31,16 +31,20 @@ class MuseumTest < Minitest::Test
     @dmns.add_exhibit(@dead_sea_scrolls)
     @dmns.add_exhibit(@imax)
 
-    assert_equal [@gems_and_minerals, @dead_sea_scrolls, @imax]
+    assert_equal [@gems_and_minerals, @dead_sea_scrolls, @imax], @dmns.exhibits
   end
 
   def test_exhibit_recommendation
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
     @bob.add_interest("Dead Sea Scrolls")
     @bob.add_interest("Gems and Minerals")
     @sally.add_interest("IMAX")
 
     assert_equal [@gems_and_minerals, @dead_sea_scrolls], @dmns.recommend_exhibits(@bob)
-    assert_equal [@imax], @dmns.reccomend_exhibits(@sally)
+    assert_equal [@imax], @dmns.recommend_exhibits(@sally)
   end
 
 end
