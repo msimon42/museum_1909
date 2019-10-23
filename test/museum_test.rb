73d1fa8d89c6452df909b2511a68d8b8f6cf49db
msimon42 <msimon@uab.edu>
2019-10-23 09:33:34 -0600

    Add test suite for museum

---
+++ test/museum_test.rb
@@ -0,0 +1,44 @@
+require 'minitest'
+require 'minitest/autorun'
+require_relative '../lib/museum'
+
+class MuseumTest < Minitest::Test
+  def setup
+    @dmns = Museum.new("Denver Museum of Nature and Science")
+    @gems_and_minerals = Exhibit.new("Gems and Minerals", 0)
+    @dead_sea_scrolls = Exhibit.new("Dead Sea Scrolls", 10)
+    @imax = Exhibit.new("IMAX", 15)
+    @bob = Patron.new('Bob', 20)
+    @sally = Patron.new('Sally', 20)
+  end
+
+  def test_existence
+    assert_instance_of Museum, @dmns
+  end
+
+  def test_name
+    assert_equal 'Denver Museum of Nature and Science', @dmns.name
+  end
+
+  def test_exhibits
+    assert_equal [], @dmns.exhibits
+  end
+
+  def test_add_exhibit
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    assert_equal [@gems_and_minerals, @dead_sea_scrolls, @imax]
+  end
+
+  def test_exhibit_recommendation
+    @bob.add_interest("Dead Sea Scrolls")
+    @bob.add_interest("Gems and Minerals")
+    @sally.add_interest("IMAX")
+
+    assert_equal [@gems_and_minerals, @dead_sea_scrolls], @dmns.recommend_exhibits(@bob)
+    assert_equal [@imax], @dmns.reccomend_exhibits(@sally)
+  end   
+
+end
